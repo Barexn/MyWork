4e4046b660e67fe07696167dd93525dd66cb8e5c
yqq <2687698398@qq.com>
2017-11-27 19:36:47 +0800

    mmmmmmm

--- app/src/main/java/mywork/mywork/MainActivity.java
+++ app/src/main/java/mywork/mywork/MainActivity.java
@@ -3,11 +3,13 @@ package mywork.mywork;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //System.out.println("======="+MyUtil.add(2,3));
     }
 }
---
+++ mylibrary/src/main/java/mywork/mylibrary/MyUtil.java
@@ -0,0 +1,11 @@
+package mywork.mylibrary;
+
+/**
+ * Created by Bare on 2017/11/27.
+ */
+
+public class MyUtil {
+    public static int add(int i,int j){
+        return i+j;
+    }
+}
